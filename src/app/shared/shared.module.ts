a9cba85ca3517f46a35708c9db1f02d2c7e21c0a
Piyush Pushkar <piyush.mvit13@gmail.com>
2020-05-04 01:44:47 +0530

    Updating project to Angular 9

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatButtonModule, MatTabsModule, MatIconModule, MatDialogModule, MatOptionModule,
-  MatFormFieldModule, MatSelectModule, MatSnackBarModule, MatSidenavModule, MatAutocompleteModule } from '@angular/material';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { FormsModule } from '@angular/forms';
 import {DragDropModule} from '@angular/cdk/drag-drop';
@@ -15,7 +23,6 @@ import {DragDropModule} from '@angular/cdk/drag-drop';
     MatTabsModule,
     MatIconModule,
     MatDialogModule,
-    MatOptionModule,
     MatFormFieldModule,
     MatSelectModule,
     FormsModule,
@@ -30,7 +37,6 @@ import {DragDropModule} from '@angular/cdk/drag-drop';
     MatTabsModule,
     MatIconModule,
     MatDialogModule,
-    MatOptionModule,
     MatFormFieldModule,
     MatSelectModule,
     FormsModule,
